6c756d9a882d18264106346a36f759f462c7bfdb
Lasat <61060252+Himezon@users.noreply.github.com>
2023-11-11 22:24:57 +0300

    YandexMapService

--- task-service/src/main/java/ru/lct/itmoteam/taskservice/service/YandexMapService.java
+++ task-service/src/main/java/ru/lct/itmoteam/taskservice/service/YandexMapService.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
@@ -13,6 +15,7 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Service
@@ -27,13 +30,13 @@ public class YandexMapService {
 
         return restTemplate.getForObject(url, String.class);
     }
-    private static List<String> getAddressesValuesFromJSON(String xml, String value){
+    private static List<String> getAddressesValuesFromJSON(String xml, String type){
         List<String> textValues = new ArrayList<>();
         try {
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = factory.newDocumentBuilder();
             Document document = builder.parse(new InputSource(new StringReader(xml)));
-            NodeList nodeList = document.getElementsByTagName(value);
+            NodeList nodeList = document.getElementsByTagName(type);
             for (int i = 0; i < nodeList.getLength(); i++) {
                 Element element = (Element) nodeList.item(i);
                 String text = element.getTextContent();
@@ -87,11 +90,42 @@ public class YandexMapService {
         return "https://yandex.ru/maps/35/krasnodar/?ll=39.026047%2C45.031821&mode=routes&rtext=45.012835%2C39.071711~45.044960%2C38.977047";
     }
 
-    public int getMeanTimeBetweenTwoAddresses(String address1, String address2) {
-        // TODO: возвращает время езды на машине между ними в минутах по двум существующим адресам
-        //  гарантируется, что адреса существуют на карте мира
-        //  должно использовать final таблицу или хэшмапу, а адреса могут быть как текстом, так и айдишниками
-        return 36;
+    public HashMap<String, Integer> getMeanTimeBetweenTwoAddresses(String address1, String address2) {
+        String geocode1 = getAddressesValuesFromJSON(getJsonFromUrl(UriComponentsBuilder.fromHttpUrl(GEOCODE_ENDPOINT)
+                .queryParam("apikey", API_KEY)
+                .queryParam("geocode", address1.replace(" ", "+"))
+                .queryParam("region", "russia")
+                .queryParam("bbox", "19.642601,41.185207~190.10042,81.25044")
+                .queryParam("results", "1")
+                .build().toUriString()), "pos").get(0).replace(" ", "+");
+        String geocode2 = getAddressesValuesFromJSON(getJsonFromUrl(UriComponentsBuilder.fromHttpUrl(GEOCODE_ENDPOINT)
+                .queryParam("apikey", API_KEY)
+                .queryParam("geocode", address2.replace(" ", ","))
+                .queryParam("region", "russia")
+                .queryParam("bbox", "19.642601,41.185207~190.10042,81.25044")
+                .queryParam("results", "1")
+                .build().toUriString()), "pos").get(0).replace(" ", ",");
+        String response = getJsonFromUrl(UriComponentsBuilder.fromHttpUrl("https://graphhopper.com/api/1/matrix")
+                        .queryParam("key", "530a162f-ce50-4a51-91c0-ab6408e1294f")
+                        .queryParam("point", geocode1)
+                        .queryParam("point", geocode2)
+                        .queryParam("profile", "car")
+                        .queryParam("out_array", "times")
+                        .build().toUriString());
+        HashMap<String, Integer> resultMap = new HashMap<>();
+        JSONObject jsonObject = new JSONObject(response);
+        JSONArray timesArray = jsonObject.getJSONArray("times");
+        for (int i = 0; i < timesArray.length(); i++) {
+            JSONArray innerArray = timesArray.getJSONArray(i);
+            for (int j = 0; j < innerArray.length(); j++) {
+                if (i != j) {
+                    int timeInMinutes = innerArray.getInt(j) / 60;
+                    String key = address1 + " -> " + address2;
+                    resultMap.put(key, timeInMinutes);
+                }
+            }
+        }
+        return resultMap;
     }
 
     public int getTimeBetweenTwoAddressesWithDaytime(String address1, String address2, int daytime) {
