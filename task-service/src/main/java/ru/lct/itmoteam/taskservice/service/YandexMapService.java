8bc661b9840b69480add54f37c426161ba7e63a7
Lasat <61060252+Himezon@users.noreply.github.com>
2023-11-11 11:28:46 +0300

    YandexMapService

--- task-service/src/main/java/ru/lct/itmoteam/taskservice/service/YandexMapService.java
+++ task-service/src/main/java/ru/lct/itmoteam/taskservice/service/YandexMapService.java
@@ -2,22 +2,82 @@ package ru.lct.itmoteam.taskservice.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class YandexMapService {
     @Autowired
     LocationService locationService;
+    private static final String API_KEY = "";
+    private static final String GEOCODE_ENDPOINT = "https://geocode-maps.yandex.ru/1.x";
+    private static final String ROUTING_ENDPOINT = "https://api.routing.yandex.net/v2/route";
+    public String getJsonFromUrl(String url) {
+        RestTemplate restTemplate = new RestTemplate();
 
+        return restTemplate.getForObject(url, String.class);
+    }
+    private static List<String> getAddressesValuesFromJSON(String xml, String value){
+        List<String> textValues = new ArrayList<>();
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(new InputSource(new StringReader(xml)));
+            NodeList nodeList = document.getElementsByTagName(value);
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Element element = (Element) nodeList.item(i);
+                String text = element.getTextContent();
+                textValues.add(text);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return textValues;
+    }
     public List<String> getAddressesByInput(String input) {
         // TODO: по входным значениям добывает не более 5 адресов, которые наиболее похожи на введённый
-        return List.of(new String[]{"Краснодар, Красная, д. 139", "Краснодар, В.Н. Мачуги, 41", "Краснодар, Красных Партизан, 321"});
+        String geocode_uri = UriComponentsBuilder.fromHttpUrl(GEOCODE_ENDPOINT)
+                .queryParam("apikey", API_KEY)
+                .queryParam("geocode", input.replace(" ", "+"))
+                .queryParam("region", "russia")
+                .queryParam("bbox", "19.642601,41.185207~190.10042,81.25044")
+                .queryParam("results", "5")
+                .build().toUriString();
+        String json = getJsonFromUrl(geocode_uri);
+        return getAddressesValuesFromJSON(json, "text");
     }
 
     public String getLinkOfRouteByTwoAddresses(String address1, String address2) {
         // TODO: по двум существующим адресам возвращает ссылку на маршрут между ними в Яндекс картах
         //  гарантируется, что адреса существуют на карте мира
+//        String geocode_uri1 = getAddressesValuesFromJSON(getJsonFromUrl(UriComponentsBuilder.fromHttpUrl(GEOCODE_ENDPOINT)
+//                .queryParam("apikey", API_KEY)
+//                .queryParam("geocode", address1.replace(" ", "+"))
+//                .queryParam("region", "russia")
+//                .queryParam("bbox", "19.642601,41.185207~190.10042,81.25044")
+//                .queryParam("results", "1")
+//                .build().toUriString()), "pos").get(0).replace(" ", "+");
+//        String geocode_uri2 = getAddressesValuesFromJSON(getJsonFromUrl(UriComponentsBuilder.fromHttpUrl(GEOCODE_ENDPOINT)
+//                .queryParam("apikey", API_KEY)
+//                .queryParam("geocode", address2.replace(" ", ","))
+//                .queryParam("region", "russia")
+//                .queryParam("bbox", "19.642601,41.185207~190.10042,81.25044")
+//                .queryParam("results", "1")
+//                .build().toUriString()), "pos").get(0).replace(" ", ",");
+//        String route_uri = UriComponentsBuilder.fromHttpUrl(ROUTING_ENDPOINT)
+//                .queryParam("apikey", API_KEY)
+//                .queryParam("waypoints", geocode_uri1+"|"+geocode_uri2)
+//                .build().toUriString();
         return "https://yandex.ru/maps/35/krasnodar/?ll=39.026047%2C45.031821&mode=routes&rtext=45.012835%2C39.071711~45.044960%2C38.977047";
     }
 
--- task-service/src/test/java/ru/lct/itmoteam/taskservice/TaskServiceApplicationTests.java
+++ task-service/src/test/java/ru/lct/itmoteam/taskservice/TaskServiceApplicationTests.java
@@ -1,13 +1,68 @@
 package ru.lct.itmoteam.taskservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
+@Service
 class TaskServiceApplicationTests {
+	public String getJsonFromUrl(String url) {
+		RestTemplate restTemplate = new RestTemplate();
 
-	@Test
-	void contextLoads() {
+		return restTemplate.getForObject(url, String.class);
 	}
-
+	private static final String API_KEY = "067cdf79-c9bc-476e-97de-8de349a232d6";
+	private static final String GEOCODE_ENDPOINT = "https://geocode-maps.yandex.ru/1.x";
+	private static final String ROUTING_ENDPOINT = "https://api.routing.yandex.net/v2/route";
+	public static List<String> getTextValuesFromXML(String xml, String pos){
+		List<String> textValues = new ArrayList<>();
+		try {
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document document = builder.parse(new InputSource(new StringReader(xml)));
+			NodeList nodeList = document.getElementsByTagName(pos);
+			for (int i = 0; i < nodeList.getLength(); i++) {
+				Element element = (Element) nodeList.item(i);
+				String text = element.getTextContent();
+				textValues.add(text);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return textValues;
+	}
+	public static void main(String[] args) {
+		TaskServiceApplicationTests taskServiceApplicationTests = new TaskServiceApplicationTests();
+		String geocode_uri1 = getTextValuesFromXML(taskServiceApplicationTests.getJsonFromUrl(UriComponentsBuilder.fromHttpUrl(GEOCODE_ENDPOINT)
+				.queryParam("apikey", API_KEY)
+				.queryParam("geocode", "142.03519 47.33614".replace(" ", ","))
+				.queryParam("region", "russia")
+				.queryParam("bbox", "19.642601,41.185207~190.10042,81.25044")
+				.queryParam("results", "1")
+				.build().toUriString()), "pos").get(0).replace(" ", "+");
+		String geocode_uri2 = getTextValuesFromXML(taskServiceApplicationTests.getJsonFromUrl(UriComponentsBuilder.fromHttpUrl(GEOCODE_ENDPOINT)
+				.queryParam("apikey", API_KEY)
+				.queryParam("geocode", "5.143367 60.425573".replace(" ", ","))
+				.queryParam("region", "russia")
+				.queryParam("bbox", "19.642601,41.185207~190.10042,81.25044")
+				.queryParam("results", "1")
+				.build().toUriString()), "pos").get(0).replace(" ", ",");
+		String route_uri = UriComponentsBuilder.fromHttpUrl(ROUTING_ENDPOINT)
+				.queryParam("apikey", API_KEY)
+				.queryParam("waypoints", geocode_uri1+"|"+geocode_uri2)
+				.build().toUriString();
+		System.out.println(route_uri);
+	}
 }
